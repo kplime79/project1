54370937fb75bf7c06a1af72b4d0922142c0f5ea
kplime79 <kplime79@gmail.com>
2025-10-08 23:29:27 +0900

    Title: 체질량지수, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251008/체질량지수/bmi.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int h, w;
+    scanf("%d %d", &h, &w);
+    int b;
+    b = (10000*w)/(h*h);
+    printf("%d\n", b);
+
+    if (b >= 25)
+    {
+        printf("Obesity");
+    }
+    return 0;
+}
