a6e20d07e37f299713927fffae3896e57da06641
kplime79 <kplime79@gmail.com>
2025-10-15 16:37:47 +0900

    Title: 지그재그로 정수 채우기, Time: 9ms, Memory: 0MB, Status: Passed - Codetree

--- 251015/지그재그로 정수 채우기/zigzag-numbering.c
+++ 251015/지그재그로 정수 채우기/zigzag-numbering.c
@@ -1,33 +1,36 @@
-#include <stdio.h>
+#include<stdio.h>
 
 int main() {
-    int b=0,n=0,m=0,c=0;
-    int a[100][100];
-    scanf("%d %d",&n,&m);
-    c = (n*m-1);
-    for (int i=0;i<n;i++)
-    {
-        for (int j=0;j<m;j++)
-        {
-            if (j%2!=0)
-            {
-                a[i][j] = c--;
+    // 변수 선언:
+    int n, m;
+    int answer[100][100] = {0};
+
+    // 입력:
+    scanf("%d %d", &n, &m);
+    
+    // Step 1:
+    int count = 0;
+    for(int col = 0; col < m; col++) {
+        if (col % 2 == 0){
+            // Case 1:
+            for(int row = 0; row < n; row++){
+                answer[row][col] = count;
+                count++;
             }
-            else
-            {
-                a[i][j] = b++;
+        }
+        else {
+            // Case 2:
+            for(int row = n - 1; row >= 0; row--){
+                answer[row][col] = count;
+                count++;
             }
         }
     }
-
-
-
-    for (int i=0;i<n;i++)
-    {
-        for (int j=0;j<m;j++)
-        {
-            printf("%d ",a[i][j]);
-        }
+    
+    // 출력:
+    for(int row = 0; row < n; row++) {
+        for(int col = 0; col < m; col++) 
+            printf("%d ", answer[row][col]);
         printf("\n");
     }
     return 0;
