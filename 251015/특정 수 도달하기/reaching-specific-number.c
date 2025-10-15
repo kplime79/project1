7aa167bd802274f8bb212ca8a77f1b96ac43d785
kplime79 <kplime79@gmail.com>
2025-10-15 09:29:28 +0900

    Title: 특정 수 도달하기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 251015/특정 수 도달하기/reaching-specific-number.c
+++ 251015/특정 수 도달하기/reaching-specific-number.c
@@ -8,7 +8,13 @@ int main() {
         scanf(" %d",&a[i]);
     }
     for (int i=0;i<10;i++)
-    {
+    {   
+        
+        if (a[0]==0){
+        printf("%d %d",0,0);
+        break;
+        }
+
         if (a[i]>=250)
         {
             printf("%d %.1f",b,(double)b/i);
